3f874124509bf5d0672401983ed333989d58e85f
hint1k <filbad17@gmail.com>
2025-01-28 19:24:23 +0400

    4th commit Changes: 1) Passing an array to a specific route 2) Checking the content of array using loop 3) Create pages for every element of array.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,25 @@
-<?php
+<h1>
+    The list of tasks.
+</h1>
+<br>
+{{--@isset($name)--}}
+{{--The name is: {{$name}}. --}}{{--this code will only be displayed if $name is defined--}}
+{{--@endisset--}}
+<div>
+    {{--    @if(count($tasks)>0)--}}
+    {{--        @foreach($tasks as $task)--}}
+    {{--            <div>--}}
+    {{--            {{$task->title}} and {{$task->description}};--}}
+    {{--            </div>--}}
+    {{--        @endforeach--}}
+    {{--    @else--}}
+    {{--        <div>There are no tasks!</div>--}}
+    {{--    @endif--}}
+    @forelse($tasks as $task)
+        <div>
+             <a href="{{route('tasks.show', ['id'=>$task->id])}}"> {{$task->title}} </a>
+        </div>
+    @empty
+        No tasks
+    @endforelse
+</div>
