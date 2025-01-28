bdedf03df71f84ce20127c707052e3521fb7245d
hint1k <filbad17@gmail.com>
2025-01-28 17:52:27 +0400

    2nd commit Changes: 1) Cleared readme file 2) Cleared cache using command: php artisan route:clear so now the app shows empty starting page instead of default page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "main page";
+    return 'main page';
     //    return view('welcome');
 });
 
