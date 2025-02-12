3f165d0cd7f4b994542c14cb72543ca2a2eed01e
hint1k <filbad17@gmail.com>
2025-02-12 16:42:12 +0400

    7th commit Changes: 1) created create.blade.php view 2) created a route to save records in database 3) added edit.blade.php view 4) created route to update records in database 5) added validation of records before saving in database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,17 @@
 <html>
 <head>
     <title>Laravel Task List App</title>
+    @yield('styles')
 </head>
 <body>
 <h1>@yield('title')</h1>
 <div>
-@yield('content')
+    @if(session()->has('success'))
+        <div>
+            {{session('success')}}
+        </div>
+    @endif
+    @yield('content')
 </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Task;
 
@@ -9,22 +9,56 @@ Route::get('/', function () {
 })->name('main');
 
 Route::get('/tasks', function () {
-/*
-console commands: php artisan tinker and \App\Models\Task::all(); and
-\App\Models\Task::select('id','title')->where('completed',true)->get();
-*/
     $tasks = Task::latest()->where('completed', true)->get();
     return view('index', ['tasks' => $tasks]); //shows 'index' page from resources/views
 })->name('tasks.index');
 
+//this route has to be ahead of the next one or "create" will be read as "{id}"
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
-//    $task = collect($tasks)->firstWhere('id', $id);
-//    if (!$task) {
-//        abort(Response::HTTP_NOT_FOUND);
-//    }
     $taskId = Task::findOrFail($id);
     return view('show', ['task' => $taskId]);
 })->name('tasks.show');
+
+Route::get('/tasks/{id}/edit', function ($id) {
+    $taskId = Task::findOrFail($id);
+    return view('edit', ['task' => $taskId]);
+})->name('tasks.edit');
+
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = new Task();
+    $task->title=$data['title'];
+    $task->description=$data['description'];
+    $task->long_description=$data['long_description'];
+    $task->save(); // create a new record in mysql database
+
+    return redirect()->route('tasks.show',['id'=>$task->id])
+        ->with('success','Task created successfully!'); // one time only message
+})->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title=$data['title'];
+    $task->description=$data['description'];
+    $task->long_description=$data['long_description'];
+    $task->save(); // create a new record in mysql database
+
+    return redirect()->route('tasks.show',['id'=>$task->id])
+        ->with('success','Task updated successfully!'); // one time only message
+})->name('tasks.update');
 
 Route::fallback(function () {
     return 'This is a fallback page';
